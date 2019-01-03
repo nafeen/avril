35fee4f865fad982bd9d4058a412b9e372fb3d38
Abdul Nafeen <abdulnafeen@yahoo.com>
2019-01-03 21:24:03 +0530

    ADDED a front-end for displaying sample IEX API response.
    [TODO] Accept form data and make API request accordingly

--- server.js
+++ server.js
@@ -1,6 +1,55 @@
 import { IEXClient } from 'iex-api'
 import _fetch from 'isomorphic-fetch'
 
-const iex = new IEXClient(_fetch)
-iex.stockCompany('FB')
-  .then(company => console.log(company))
+/**********************************************************************************************
+ *
+ * THIS SCRIPT CREATES THE WEB SERVER FOR RUNNING IEX APP
+ *
+ ********************************************************************************************** */
+
+
+/* ============================================================================================
+ * TODO:
+ *
+/* ============================================================================================ */
+
+
+// ADDING middlewares and other dependencies
+var express            =   require('express');
+var app                =   express();
+
+
+// Auto-redirect HTTP requests to HTTPS
+// app.use(function(req, res, next) { 
+//     // console.log("|| Request Log || ", req.get('X-Forwarded-Proto'), "/", req.get('Host'), req.url);
+//     if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'].toLowerCase() === 'http') {
+//         console.log("Redirecting HTTP to HTTPS");
+//         return res.redirect('https://' + req.headers.host + req.url);
+//     }
+//     else {
+//         next();
+//     }
+// });
+
+
+// Routing for IEX App
+app.use('/', express.static('./apps/iex-app'));
+
+app.post('/post-data', function(req, res) {
+	const iex = new IEXClient(_fetch)
+	iex.stockCompany('FB')
+		.then(company => res.send(company));
+});
+
+
+// listening on port 3000
+app.listen(3000);
+
+
+// Logging status to console
+console.log("--------------------------------------------------");
+console.log("----- IEX app server running at port 3000 -----");
+console.log("--------------------------------------------------");
+
+/*********************************************************************************************/
+/*********************************************************************************************/
